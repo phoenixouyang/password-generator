c967a024d60fc4652039073f34a8fcd94e1c6e0f
Pholobo <ouyangphoenix@gmail.com>
2023-01-11 15:05:50 -0500

    add prompt boxes and confirm boxes for password criteria.

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,25 @@
-// Assignment code here
+// Generate strings for password criteria
 const lowerAlpha = "abcdefghijklmnopqrstuvwxyz";
 const upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
-const specialChar = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+const specialChar = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-let passLength = prompt("Please enter your desired password length (between 8 and 128 characters)");
-if (passLength < 8|| passLength > 128 || passLength === undefined) {
-  alert("Please enter a password length between 8 and 128 characters")
+// Get user input for password length
+let passLength = parseInt(window.prompt("Please enter your desired password length (between 8 and 128 characters)"));
+
+// Validate user input for password length
+if (passLength < 8|| passLength > 128 || !passLength) {
+  passLength = parseInt(window.prompt("You did not enter a valid number. Please enter a length between 8 and 128 characters"));
 }
+// Get user input for password characters
+else {
+  let lowerAlphaConfirm = window.confirm("Do you want lower case letters in your password? Click 'OK' for yes, 'Cancel' for no.");
+  let upperAlphaConfirm = window.confirm("Do you want upper case letters in your password? Click 'OK' for yes, 'Cancel' for no.");
+  let numbersConfirm = window.confirm("Do you want numbers in your password? Click 'OK' for yes, 'Cancel' for no.");
+  let specialCharConfirm = window.confirm("Do you want special characters in your password? Click 'OK' for yes, 'Cancel' for no.");
+};
+
+
 
 
 // Get references to the #generate element
